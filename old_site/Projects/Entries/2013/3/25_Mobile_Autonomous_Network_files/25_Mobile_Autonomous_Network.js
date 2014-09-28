d50f10dab0364705d72a291420d8c78eb1d4bdfa
Dennis Wai <dwai213@gmail.com>
2014-09-28 11:57:08 -0700

    Updated website pages and added old site folder

---
+++ old_site/Projects/Entries/2013/3/25_Mobile_Autonomous_Network_files/25_Mobile_Autonomous_Network.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20130327
+
+setTransparentGifURL('../../../../Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-2,2,4,213),url:'25_Mobile_Autonomous_Network_files/stroke.png'},{rect:new IWRect(-2,-2,4,4),url:'25_Mobile_Autonomous_Network_files/stroke_1.png'},{rect:new IWRect(2,-2,416,4),url:'25_Mobile_Autonomous_Network_files/stroke_2.png'},{rect:new IWRect(418,-2,4,4),url:'25_Mobile_Autonomous_Network_files/stroke_3.png'},{rect:new IWRect(418,2,4,213),url:'25_Mobile_Autonomous_Network_files/stroke_4.png'},{rect:new IWRect(418,215,4,4),url:'25_Mobile_Autonomous_Network_files/stroke_5.png'},{rect:new IWRect(2,215,416,4),url:'25_Mobile_Autonomous_Network_files/stroke_6.png'},{rect:new IWRect(-2,215,4,4),url:'25_Mobile_Autonomous_Network_files/stroke_7.png'}],new IWSize(420,217))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('25_Mobile_Autonomous_Network_files/25_Mobile_Autonomous_NetworkMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../../../../Media/transparent.gif');BlogFixupPreviousNext();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
