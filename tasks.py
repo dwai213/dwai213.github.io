e257bba8ebd4c48843e4f34b027b4689ad3985d5
Dennis Wai <dwai213@gmail.com>
2020-04-09 01:02:26 -0700

    Adding new deploy task

--- tasks.py
+++ tasks.py
@@ -6,6 +6,8 @@
 from invoke import Collection, task
 import cmd
 from colorama import Fore, Style
+from datetime import datetime
+import sys
 
 
 @task()
@@ -17,6 +19,48 @@ def build(c):
           "Doing one time build of Jekyll site..." +
           Style.RESET_ALL)
     cmd.jekyll.build(c)
+
+
+def push_to_server(c, msg, branch):
+    '''
+    Creates a commit and then push to master w/ error handling
+    '''
+    c.run("git add -u")
+    res = c.run('git commit -m "%s"' % msg)
+    res = c.run("git push origin %s" % branch)
+    if res.exit_code() != 0:
+        print(Fore.RED +
+              "Unable to deploy site to %s" % branch +
+              Style.RESET_ALL)
+        print(res.stdout)
+        print(res.stderr)
+        sys.exit(1)
+
+
+@task()
+def deploy(c):
+    '''
+    Copies current Jekyll src and puts in submodule and git push
+    '''
+    print(Fore.GREEN +
+          "Deploying site..." +
+          Style.RESET_ALL)
+    # Figure out if there are uncommitted changes in src before proceeding
+    res = c.run("git status --porcelain | grep -v site", hide=True, warn=True)
+    if res and len(res.stdout) > 0:
+        # Implies that there are uncommitted changes
+        print(Fore.RED +
+              "You have uncommitted changes\n" +
+              "Address them before deploying." +
+              Style.RESET_ALL)
+        sys.exit(1)
+
+    cmt_msg = "Deployed site on " + datetime.now().strftime("%m-%d-%Y %H:%M:%S")
+    c.run("cp -r src/* site/")
+    with c.cd("site"):
+        push_to_server(c, cmt_msg, "master")
+    # This will add the update to the submodule and commit it as well
+    push_to_server(c, cmt_msg, "master-src")
 
 
 @task()
@@ -31,7 +75,7 @@ def serve(c):
 
 
 @task()
-def bundle(c):
+def prereqs(c):
     '''
     Installs all the gems
     '''
@@ -45,4 +89,5 @@ def bundle(c):
 ns = Collection()
 ns.add_task(build)
 ns.add_task(serve)
-ns.add_task(bundle)
+ns.add_task(prereqs)
+ns.add_task(deploy)
